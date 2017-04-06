2d97e1efa84b8d0aceaedc1d4b326ad092cbe05d
Nilson <nlj@cin.ufpe.br>
2017-04-06 20:22:02 -0300

    tags eliminated. stranges characters refers to utf-8 codification. python can handle it.

---
+++ preprocessor.py
@@ -0,0 +1,32 @@
+import json
+import re
+import os
+
+macro = "docbase/original/macroeconomics/"
+bio = "docbase/original/political_leader_biographies/"
+
+def process(path):
+    length = len(os.listdir(path))
+
+    for i in range(length):
+        with open(path + str(i) + ".json", "r") as w:
+            f = json.load(w)
+
+        content = re.sub("<[^>]*>", "", f["content"])
+        title = re.sub("<[^>]*>", "", f["title"])
+
+        f = {"title": title, "content": content}
+
+        with open(path + str(i) +".json", "w") as w:
+            json.dump(f, w)
+
+    return
+
+def main( ):
+    process(macro)
+    process(bio)
+    
+    return
+
+if __name__ == '__main__':
+    main( )
